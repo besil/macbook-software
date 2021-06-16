17b5f9e811e1fb5a90052f3fbbaf9b94eb2a7918
besil <besil@snow.local>
2021-06-16 22:28:26 +0200

    First commit

---
+++ install.sh
@@ -0,0 +1,27 @@
+# dev environment
+brew install --cask visual-studio-code
+brew install --cask dbeaver-community
+brew install --cask jetbrains-toolbox
+brew install git
+brew install git-gui
+brew install --cask docker
+
+# dev tools
+brew install postgresql
+
+# cloud
+brew install awscli
+
+# languages
+brew install python@3.9
+brew install adoptopenjdk
+brew install kotlin
+
+# other
+brew install --cask firefox
+brew install --cask google-chrome
+brew install --cask transmission
+brew install --cask spotify
+brew install --cask appcleaner
+brew install --cask calibre
+brew install --cask whatsapp
