a3f9a5f1f06f6c8f18227e690832340303181cad
Silvio Bernardinello <7081486+besil@users.noreply.github.com>
2025-08-04 13:36:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,69 +1,11 @@
-# dev tools
-brew install --cask visual-studio-code
-#brew install beekeeper-studio
-#brew install --cask dbeaver-community
-#brew install --cask db-browser-for-sqlite
-brew install --cask dbgate
-brew install --cask jetbrains-toolbox
-brew install --cask insomnia
-brew install git
-brew install git-gui
-brew install --cask sourcetree
-brew install --cask docker
-brew install helm
-brew install terraform
-brew install --cask ngrok
-brew install telnet
-brew install awscli
-#brew install oci-cli
-brew install --cask cyberduck
-
-# Programming
-## python
-brew install python@3.11
-brew install python@3.12
-brew install pyenv
-brew install pipx
-pipx ensurepath
-pipx install poetry
-## node
-#brew install node
-## java
-brew tap sdkman/tap
-brew install sdkman-cli
-echo 'export SDKMAN_DIR=$(brew --prefix sdkman-cli)/libexec
-[[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"' >> ~/.zshrc
-sdk install java
-sdk install maven
-sdk install quarkus
-#brew install --cask temurin
-#brew install kotlin
-## dart and flutter
-#brew install --cask flutter
-
-# work
-brew install --cask notion
-brew install --cask obs
-#brew install --cask screen-studio
-brew install --cask google-drive
-brew install --cask slack
-brew install --cask balsamiq-wireframes
-brew install --cask openlens
-brew install k9s
-#brew install --cask twist
-brew install --cask vincelwt-chatgpt
-brew install --cask zoom
-#brew install --cask figma
-#brew install --cask clockify # time tracking
-
 # personal
 brew install --cask bitwarden # password manager
-brew install --cask adguard # safari and macbook ad block
-brew install --cask hush # automatic cookie consent for safari
+#brew install --cask adguard # safari and macbook ad block
+#brew install --cask hush # automatic cookie consent for safari
 #brew install --cask calibre # ebook reader and library
 #brew install --cask caffeine # better use amphetamine from app store
-brew install --cask displaylink # must have for 2nd monitor
-#brew install --cask obsidian # note taker. or just use notion
+#brew install --cask displaylink # must have for 2nd monitor
+brew install --cask obsidian # note taker. or just use notion
 brew install --cask firefox # best browser
 brew tap homebrew/cask-versions
 brew install --cask firefox-developer-edition
@@ -74,9 +16,9 @@ brew install --cask onedrive # office 365 suite
 brew install --cask microsoft-excel # office 365 suite
 brew install --cask microsoft-powerpoint # office 365 suite
 brew install --cask microsoft-word # office 365 suite
-brew install --cask microsoft-onenote # office 365 suite
+#brew install --cask microsoft-onenote # office 365 suite
 #brew install --cask microsoft-teams
-#brew install --cask tor-browser
+brew install --cask tor-browser
 #brew install --cask mediahuman-audio-converter
 brew install --cask logi-options-plus # for mouse and keyboard
 brew install --cask maccy # clip manager
@@ -84,7 +26,7 @@ brew install --cask maccy # clip manager
 brew install --cask rectangle # better window management
 #brew install --cask alt-tab # must have for windows-like alt-tab. Now switched to cmd+alt+`
 brew install --cask betterdisplay # for multiple resolution management on external monitors
-# brew install --cask raspberry-pi-imager # useful
+brew install --cask raspberry-pi-imager # useful
 brew install --cask transmission
 #brew install --cask spotify
 brew install --cask appcleaner # to easily uninstall hidden files from apps
@@ -99,5 +41,66 @@ brew install --cask vlc
 #brew install --cask 0-ad
 #brew install --cask openttd
 brew install --cask steam
-#brew install --cask battle-net
+brew install --cask battle-net
 brew install --cask porting-kit
+
+# dev tools
+brew install --cask visual-studio-code
+#brew install beekeeper-studio
+#brew install --cask dbeaver-community
+#brew install --cask db-browser-for-sqlite
+brew install --cask dbgate
+#brew install --cask jetbrains-toolbox
+#brew install --cask insomnia
+brew install git
+brew install git-gui
+#brew install --cask sourcetree
+brew install --cask docker
+#brew install helm
+#brew install terraform
+brew install --cask ngrok
+brew install telnet
+brew install awscli
+#brew install oci-cli
+brew install --cask cyberduck
+
+# Programming
+## python
+#brew install python@3.11
+#brew install python@3.12
+#brew install python@3.13
+brew install uv
+#brew install pyenv
+#brew install pipx
+#pipx ensurepath
+#pipx install poetry
+## node
+#brew install node
+brew install nvm
+## java
+#brew tap sdkman/tap
+#brew install sdkman-cli
+#echo 'export SDKMAN_DIR=$(brew --prefix sdkman-cli)/libexec
+#[[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"' >> ~/.zshrc
+#sdk install java
+#sdk install maven
+#sdk install quarkus
+#brew install --cask temurin
+#brew install kotlin
+## dart and flutter
+#brew install --cask flutter
+
+# work
+#brew install --cask notion
+brew install --cask obs
+#brew install --cask screen-studio
+brew install --cask google-drive
+#brew install --cask slack
+brew install --cask discord
+brew install --cask balsamiq-wireframes
+#brew install --cask openlens
+brew install k9s
+brew install --cask chatgpt
+brew install --cask zoom
+brew install --cask figma
+#brew install --cask clockify # time tracking
